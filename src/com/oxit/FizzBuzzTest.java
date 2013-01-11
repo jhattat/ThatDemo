284b88e3db9ca70172dc51c66c8508993023ad5d
Jérémie HATTAT <jhattat@gmail.com>
2013-01-11 09:37:31 +0100

    test Fail

--- src/com/oxit/FizzBuzzTest.java
+++ src/com/oxit/FizzBuzzTest.java
@@ -1,14 +1,15 @@
 package com.oxit;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.Test;
 
 public class FizzBuzzTest {
 
 	@Test
-	public void configIsOK() {
-		assertTrue(true);
+	public void fizzBuzzz1_is1() {
+		assertThat(fizzBuzz(1), equalTo("1"));
 	}
 
 }
