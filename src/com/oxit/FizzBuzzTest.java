6f918e418a17146f5d28dcc799dc17b0c5ccff95
Jérémie HATTAT <jhattat@gmail.com>
2013-01-11 09:50:08 +0100

    fizzBuzz2

--- src/com/oxit/FizzBuzzTest.java
+++ src/com/oxit/FizzBuzzTest.java
@@ -12,8 +12,15 @@ public class FizzBuzzTest {
 		assertThat(fizzBuzz(1), equalTo("1"));
 	}
 
+	@Test
+	public void fizzBuzzz2_is2() {
+		assertThat(fizzBuzz(2), equalTo("2"));
+	}
+
 	private String fizzBuzz(int i) {
-		return "1";
+		if(i == 1)
+			return "1";
+		return "2";
 	}
 
 }
